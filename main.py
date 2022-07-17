3d754c9ee9513464b66811b812558f3ad2b0c567
duhan <duhan@189.cn>
2022-07-17 11:45:01 +0800

    .

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # import the pygame module, so you can use it
-import pygame
+import pygame as pg
  
 # define a main function
 def main():
      
     # initialize the pygame module
-    pygame.init()
+    pg.init()
     # load and set the logo
-    pygame.display.set_caption("minimal program")
+    pg.display.set_caption("minimal program")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((800,600))
+    screen = pg.display.set_mode((800,600))
      
     # define a variable to control the main loop
     running = True
@@ -18,11 +18,16 @@ def main():
     # main loop
     while running:
         # event handling, gets all event from the event queue
-        for event in pygame.event.get():
+        for event in pg.event.get():
             # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
+            if event.type == pg.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    running = False
+                print(event.dict['unicode'])
+                # print(pg.key.name(event.key), event.)
      
      
 # run the main function only if this module is executed as the main script
